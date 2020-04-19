84747dcd1ccdb9d17701168cc236675b53615bee
mathtkang <51039577+mathtkang@users.noreply.github.com>
2020-04-19 13:23:33 +0900

    node.js_module & javascript.js_module
    200419

---
+++ _module/Node.js_module/greeting.js
@@ -0,0 +1,3 @@
+exports.welcome = function() {
+    return 'hi'
+}
---
+++ _module/Node.js_module/index.js
@@ -0,0 +1,4 @@
+var greeting = require('./greeting')
+var printing = require('./printing')
+console.log(greeting.welcome())
+console.log(printing.print())
